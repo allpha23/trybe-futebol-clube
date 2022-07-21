696fa603214a1d66e62f1b5e0f561a71ab042c32
allpha23 <allpha23@gmail.com>
2022-07-21 09:53:54 -0300

    feat(matcheService): add finish matche

--- app/backend/src/services/matcheService.ts
+++ app/backend/src/services/matcheService.ts
@@ -14,9 +14,24 @@ const matcheList = async () => {
 };
 
 const createMatche = async (data: IMatche) => {
+  const { homeTeam, awayTeam } = data;
+
+  const findTeamHome = await Teams.findByPk(homeTeam);
+  const findTeamAway = await Teams.findByPk(awayTeam);
+
+  if (!findTeamHome || !findTeamAway) return false;
+
   const create = await Matches.create({ ...data, inProgress: true });
 
   return create;
 };
 
-export default { matcheList, createMatche };
+const finishMatche = async (id: string) => {
+  await Matches.update({ inProgress: false }, {
+    where: { id },
+  });
+
+  return { message: 'Finished' };
+};
+
+export default { matcheList, createMatche, finishMatche };
