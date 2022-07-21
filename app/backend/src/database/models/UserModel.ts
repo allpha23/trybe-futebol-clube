5ac59aa3f49e49163ae4216c9cb14883f09f5f66
allpha23 <allpha23@gmail.com>
2022-07-21 00:51:14 -0300

    feat: add user model

---
+++ app/backend/src/database/models/UserModel.ts
@@ -0,0 +1,42 @@
+import { Model, DataTypes } from 'sequelize';
+import db from '.';
+
+class User extends Model {
+  id: number;
+  username: string;
+  role: string;
+  email: string;
+  password: string;
+}
+
+User.init({
+  id: {
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+    type: DataTypes.INTEGER,
+  },
+  username: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+  role: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+  email: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+  password: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+}, {
+  underscored: true,
+  modelName: 'users',
+  sequelize: db,
+  timestamps: false,
+});
+
+export default User;
