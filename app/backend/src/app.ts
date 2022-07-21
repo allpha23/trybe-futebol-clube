0b3067ecd8a557e996b21a9a7eb99cfbafbc55c4
allpha23 <allpha23@gmail.com>
2022-07-21 03:35:07 -0300

    feat(app): add team

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,5 +1,6 @@
 import * as express from 'express';
 import login from './routes/loginRoute';
+import team from './routes/teamRoute';
 
 class App {
   public app: express.Express;
@@ -24,6 +25,7 @@ class App {
     this.app.use(express.json());
     this.app.use(accessControl);
     this.app.use('/login', login);
+    this.app.use('/teams', team);
   }
 
   public start(PORT: string | number):void {
