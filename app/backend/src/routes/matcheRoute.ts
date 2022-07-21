c212929c1fdb1d4c9b4fb6e35df5a99bf3d4c74e
allpha23 <allpha23@gmail.com>
2022-07-21 08:20:03 -0300

    feat(matcheRoute): add create matche

--- app/backend/src/routes/matcheRoute.ts
+++ app/backend/src/routes/matcheRoute.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import matcheController from '../controllers/matcheController';
+import validateToken from '../middlewares/authorization';
 
 const router = Router();
 
-router.get('/', matcheController);
+router.get('/', matcheController.matcheList);
+router.post('/', validateToken, matcheController.createMatche);
 
 export default router;
