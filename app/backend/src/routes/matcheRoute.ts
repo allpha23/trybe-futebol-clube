923e90985353652f6b00a239f789e1e2df302b2f
allpha23 <allpha23@gmail.com>
2022-07-21 09:56:01 -0300

    feat: add patch routes

--- app/backend/src/routes/matcheRoute.ts
+++ app/backend/src/routes/matcheRoute.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import matcheController from '../controllers/matcheController';
 import validateToken from '../middlewares/authorization';
+import validateMatche from '../middlewares/matcheMiddleware';
 
 const router = Router();
 
 router.get('/', matcheController.matcheList);
-router.post('/', validateToken, matcheController.createMatche);
+router.post('/', validateToken, validateMatche, matcheController.createMatche);
+router.patch('/:id/finish', matcheController.finishMatche);
 
 export default router;
