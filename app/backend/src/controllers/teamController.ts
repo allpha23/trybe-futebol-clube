3ef8f414f2e8fa4bbcbed92b6af3d8fcd25608d9
allpha23 <allpha23@gmail.com>
2022-07-21 04:41:54 -0300

    fix: retorno de team controller

--- app/backend/src/controllers/teamController.ts
+++ app/backend/src/controllers/teamController.ts
@@ -4,14 +4,14 @@ import teamService from '../services/teamService';
 const getTeams = async (req: Request, res: Response) => {
   const teams = await teamService.getTeams();
 
-  return res.status(200).json({ teams });
+  return res.status(200).json(teams);
 };
 
 const getTeamById = async (req: Request, res: Response) => {
   const { id } = req.params;
   const team = await teamService.getTeamById(id);
 
-  return res.status(200).json({ team });
+  return res.status(200).json(team);
 };
 
 export default { getTeams, getTeamById };
