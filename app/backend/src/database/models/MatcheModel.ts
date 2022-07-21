cd56b78e70ba7bbc9425c887818b0cedcafe5aeb
allpha23 <allpha23@gmail.com>
2022-07-21 00:51:45 -0300

    feat: add matche model

---
+++ app/backend/src/database/models/MatcheModel.ts
@@ -0,0 +1,54 @@
+import { Model, DataTypes } from 'sequelize';
+import db from '.';
+import Team from './TeamModel';
+
+class Match extends Model {
+  id: number;
+  homeTeam: number;
+  homeTeamGoals: number;
+  awayTeam: number;
+  awayTeamGoals: number;
+  inProgress: boolean;
+}
+
+Match.init({
+  id: {
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+    type: DataTypes.INTEGER,
+  },
+  homeTeam: {
+    allowNull: false,
+    type: DataTypes.INTEGER,
+  },
+  homeTeamGoals: {
+    allowNull: false,
+    type: DataTypes.INTEGER,
+  },
+  awayTeam: {
+    allowNull: false,
+    type: DataTypes.INTEGER,
+  },
+  awayTeamGoals: {
+    allowNull: false,
+    type: DataTypes.INTEGER,
+  },
+  inProgress: {
+    allowNull: false,
+    type: DataTypes.BOOLEAN,
+  },
+}, {
+  underscored: true,
+  modelName: 'matches',
+  sequelize: db,
+  timestamps: false,
+});
+
+Match.belongsTo(Team, { foreignKey: 'homeTeam', as: 'homeTeams' });
+Match.belongsTo(Team, { foreignKey: 'awayTeam', as: 'awayTeams' });
+
+Team.hasMany(Match, { foreignKey: 'homeTeam', as: 'homeTeams' });
+Team.hasMany(Match, { foreignKey: 'awayTeam', as: 'awayTeams' });
+
+export default Match;
