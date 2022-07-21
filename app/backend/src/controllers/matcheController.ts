ad0e06315b6940bc8a0bf9fcd4afaaf89c0b65cb
allpha23 <allpha23@gmail.com>
2022-07-21 08:18:51 -0300

    feat(matcheController): add create matche

--- app/backend/src/controllers/matcheController.ts
+++ app/backend/src/controllers/matcheController.ts
@@ -1,10 +1,18 @@
 import { Request, Response } from 'express';
+import { IMatche } from '../protocols/LoginProtocol';
 import matcheService from '../services/matcheService';
 
 const matcheList = async (req: Request, res: Response) => {
-  const matches = await matcheService();
+  const matches = await matcheService.matcheList();
 
   return res.status(200).json(matches);
 };
 
-export default matcheList;
+const createMatche = async (req: Request, res: Response) => {
+  const data: IMatche = req.body;
+  const create = await matcheService.createMatche(data);
+
+  return res.status(200).json(create);
+};
+
+export default { matcheList, createMatche };
