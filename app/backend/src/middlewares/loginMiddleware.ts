4196609d4a05754f87e25d738b53f69e81c3d6fe
allpha23 <allpha23@gmail.com>
2022-07-21 00:52:58 -0300

    feat: add login middlewares

---
+++ app/backend/src/middlewares/loginMiddleware.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Request, Response } from 'express';
+import { ILogin } from '../protocols/LoginProtocol';
+
+const message = 'All fields must be filled';
+
+const validateEmail = (req: Request, res: Response, next: NextFunction) => {
+  const { email } = req.body as ILogin;
+
+  if (email === undefined) {
+    return res.status(400).json({ message });
+  }
+
+  next();
+};
+
+const validatePassword = (req: Request, res: Response, next: NextFunction) => {
+  const { password } = req.body as ILogin;
+
+  if (password === undefined) {
+    return res.status(400).json({ message });
+  }
+
+  next();
+};
+
+export default [validateEmail, validatePassword];
