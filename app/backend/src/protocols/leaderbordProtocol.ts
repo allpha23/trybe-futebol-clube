2e7ed0c31996f990d2fba31298bf681b90e18872
allpha23 <allpha23@gmail.com>
2022-07-23 21:54:04 -0300

    feat(protocols): add IteamLeaderbord

--- app/backend/src/protocols/leaderbordProtocol.ts
+++ app/backend/src/protocols/leaderbordProtocol.ts
@@ -4,3 +4,16 @@ export interface ILeadebord {
   name: string;
   matches: Matches[];
 }
+
+export interface ITeamleaderbord {
+  name: string;
+  totalPoints: number;
+  totalGames: number;
+  totalVictories: number;
+  totalDraws: number;
+  totalLosses: number;
+  goalsFavor: number;
+  goalsOwn: number;
+  goalsBalance: number;
+  efficiency: string;
+}
