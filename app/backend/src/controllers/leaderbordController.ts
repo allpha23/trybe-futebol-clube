fcad438dcf1abaed90ed4cc6099370d14918982d
allpha23 <allpha23@gmail.com>
2022-07-23 23:33:26 -0300

    feat(leaderboardController): add filter home

--- app/backend/src/controllers/leaderbordController.ts
+++ app/backend/src/controllers/leaderbordController.ts
@@ -2,9 +2,15 @@ import { Request, Response } from 'express';
 import LeaderboardService from '../services/leaderboardService';
 
 const createLeaderboard = async (req: Request, res: Response) => {
-  const leaderBoard = await LeaderboardService();
+  const leaderBoard = await LeaderboardService.createLeaderboard();
 
   return res.status(200).json(leaderBoard);
 };
 
-export default createLeaderboard;
+const filterLeaderboardHome = async (req: Request, res: Response) => {
+  const leaderBoardHome = await LeaderboardService.filterLeaderboardHome();
+
+  return res.status(200).json(leaderBoardHome);
+};
+
+export default { createLeaderboard, filterLeaderboardHome };
